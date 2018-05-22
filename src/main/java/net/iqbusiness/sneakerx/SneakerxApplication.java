1513e047858a615976febc53d0b62485935a4e0d
Lesiba <lesiba.ramphele@gmail.com>
2018-05-22 21:40:24 +0200

    Add files via upload

---
+++ src/main/java/net/iqbusiness/sneakerx/SneakerxApplication.java
@@ -0,0 +1,12 @@
+package net.iqbusiness.sneakerx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerxApplication.class, args);
+	}
+}
