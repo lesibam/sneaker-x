41b4f23ec1804b1de17872d3f9c9a3bd0941fdca
Lesiba <lesiba.ramphele@gmail.com>
2018-05-22 22:32:06 +0200

    Create UsersController.java
    Basic Rest Controller

---
+++ src/main/java/net/iqbusiness/sneakerx/web/rest/UsersController.java
@@ -0,0 +1,17 @@
+package net.iqbusiness.sneakerx.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+RequestMapping(value = "/users")
+public class UsersController {
+
+  @RequestMapping(method = GET)
+  public String helloUser() {
+    return "Hello Sneaker Head";
+  }
+  
+}
