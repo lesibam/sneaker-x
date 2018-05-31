de6a74b6751b68cd41d2e8e4462b97b62409553e
Lesiba <lesiba.ramphele@gmail.com>
2018-05-31 08:56:49 +0200

    Create User.java

---
+++ src/main/java/net/iqbusiness/sneakerx/persistence/entity/User.java
@@ -0,0 +1,17 @@
+package net.iqbusiness.sneakerx.persistence.entity;
+
+import lombok.Getter;
+import javax.persistence.Entity;
+import javax.persistene.Id;
+
+@Getter
+@Entity
+publc class UserRole {
+  @Id
+  private Long id;
+  
+  private String username;
+  private String password; 
+  private boolean enabled;
+ 
+}
