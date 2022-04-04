dde104a3cfcace7a800670c502f2f60577321d9d
lesibam <motlaupelesiba.ramphele@absa.africa>
2022-04-04 14:33:22 +0200

    Fixing 4year old bugs and Rakheen is going crazy

--- src/main/java/net/iqbusiness/sneakerx/persistence/entity/User.java
+++ src/main/java/net/iqbusiness/sneakerx/persistence/entity/User.java
@@ -2,16 +2,19 @@ package net.iqbusiness.sneakerx.persistence.entity;
 
 import lombok.Getter;
 import javax.persistence.Entity;
-import javax.persistene.Id;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Getter
 @Entity
-public class UserRole {
+public class User {
   @Id
   private Long id;
   
   private String username;
   private String password; 
   private boolean enabled;
+
+  @OneToOne
   private UserRole userRole;
 }
