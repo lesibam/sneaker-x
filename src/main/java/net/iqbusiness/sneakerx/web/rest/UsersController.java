f6a7a7cdad79f2b24b9503a14981aeca564ed9cf
lesibam <motlaupelesiba.ramphele@absa.africa>
2022-04-04 15:07:00 +0200

    Adding Vulnerability analysis

--- src/main/java/net/iqbusiness/sneakerx/web/rest/UsersController.java
+++ src/main/java/net/iqbusiness/sneakerx/web/rest/UsersController.java
@@ -1,7 +1,14 @@
 package net.iqbusiness.sneakerx.web.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.swing.text.html.parser.Entity;
+import javax.ws.rs.QueryParam;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -9,9 +16,19 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RequestMapping(value = "/users")
 public class UsersController {
 
+  @Autowired
+  private EntityManager entityManager;
+
   @RequestMapping(method = GET)
   public String helloUser() {
     return "Hello Sneaker Head";
   }
-  
+
+  @GetMapping("/vulnerability")
+  public String runSQLQuery(@QueryParam("sqlQuery") String sqlQuery) {
+    Query namedQuery = entityManager.createNamedQuery(sqlQuery);
+    entityManager.flush();
+    return namedQuery.toString();
+  }
+
 }
